b2d362b96196e70c5f8c2c8833ebda2e99480b40
Josh Boyle <joshua.boyle@accenture.com>
2020-03-21 14:57:12 -0400

    Rewrite

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,38 +1,28 @@
 (function($) {
-
-  function ContentModel() {
-    this.content = {
-      'hero_homepage.title.value': '',
-      'hero_homepage.description.value': '',
-      'homepage_section_1.title.value': '',
-      'homepage_section_1.body.value': '',
-      'homepage_section_2.title.value': '',
-      'homepage_section_2.body.value': '',
-      'guidelines_section_1.title.value': '',
-      'guidelines_section_1.body.value': '',
-      'guidelines_section_2.title.value': '',
-      'guidelines_section_2.body.value': '',
-      'farm_locations_section_1.title.value': '',
-      'farm_locations_section_1.subtitle.value': '',
-      'farm_locations_section_1.body.value': '',
-      'network_map_section_1.title.value': '',
-      'network_map_section_1.body.value': '',
-      'network_map_section_2.title.value': '',
-      'network_map_section_2.body.value': '',
-      'network_map.image.value.url': '',
-      'network_map.image.value.description': '',
-      'blinksecs.image.value.url': '',
-      'blinksecs.image.value.description': ''
-    };
-  }
-
-  var model = new ContentModel();
+  let model = {
+    'tutorialList': [],
+    'discordList': [],
+    'streamList': [],
+    'youtubeChannelList': [],
+    'usefulLinkList': []
+  };
   $.get("https://deliver.kontent.ai/a4f95819-8594-0090-4d5a-9046d8d19c69/items", function(data) {
     $.each(data.items, function(index, item) {
-      model.content[item.system.codename] = item.elements;
+      let name = item.system.codename;
+      if (name.includes('tutorial_')) {
+        model.tutorialList.push(item.elements);
+      } else if (name.includes('stream_')) {
+        model.streamList.push(item.elements);
+      } else if (name.includes('useful_link_')) {
+        model.usefulLinkList.push(item.elements);
+      } else if (name.includes('youtube_channel_')) {
+        model.youtubeChannelList.push(item.elements);
+      } else {
+        model[name] = item.elements;
+      }
     });
   }).done(function () {
     ko.applyBindings(model);
+    window.dispatchEvent(new Event('data-ready'));
   });
-
 })(jQuery);
