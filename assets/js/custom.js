57e53504012fc556f0e1fdcc8680379dc47f7c1b
Josh Boyle <joshua.boyle@accenture.com>
2020-03-22 21:44:18 -0400

    Added shopping and explorer links

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,6 +6,11 @@
     'youtubeChannelList': [],
     'usefulLinkList': []
   };
+
+  // GOOGLE API CODE
+  var CLIENT_ID = '437633750042-60rilasgddo1m8ovvfnlpkak3or178eh.apps.googleusercontent.com';
+  var API_KEY = 'AIzaSyD8U-s4VcgBHNfvR4aztOwWaVjHaFe9Q3o';
+
   $.get("https://deliver.kontent.ai/a4f95819-8594-0090-4d5a-9046d8d19c69/items", function(data) {
     $.each(data.items, function(index, item) {
       let name = item.system.codename;
@@ -23,7 +28,9 @@
     });
   }).done(function () {
     ko.applyBindings(model);
+
     window.dispatchEvent(new Event('data-ready'));
     document.getElementById("bgVideo").load();
   });
+
 })(jQuery);
